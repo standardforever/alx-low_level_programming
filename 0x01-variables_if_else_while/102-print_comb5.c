d720cb7f55bb93194954d1dd90a068c1c1ec68a4
standardforever <standard.forever123@gmail.com>
2022-04-29 00:46:32 -0700

    updated

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,35 +10,28 @@
  */
 int main(void)
 {
-	int i, j, k, l;
+	int i, j;
 
 	/* set the range of value for (1st, 2nd, 3rd, 4th)digit */
-	for (i = 48; i <= 57; i++)
+	for (i = 0; i <= 100; i++)
 	{
-		for (j = 48; j <= 57; j++)
+		for (j = 0; j <= 100; j++)
 		{
-			for (k = 48; k <= 57; k++)
+		        /* check if (i is less than j) digit */
+			/*if YES print the digit*/
+       			if (i < j)
 			{
-				for (l = 49; l <= 57; l++)
-				{
-				 /* check if (2nd >= 4th or 1st > 3rd)digit */
-				     /*if YES skip the digit(don't print)*/
-					if (j >= l || i > k)
-						continue;
-					/* if NO print */
-					putchar(i);
-					putchar(j);
-					putchar(' ');
-					putchar(k);
-					putchar(l);
-       				/* Don't print ',' & ' ' at the last count*/
-					if (i == 57 && j == 56)
-						if (k == 57 && l == 57)
-							break;
-					putchar(',');
-					putchar(' ');
-				}
+				putchar((i / 10) + '0');
+              			putchar((i % 10) + '0');
+	       			putchar(' ');
+				putchar((j / 10) + '0');
+			      	putchar((j / 10) + '0');
 			}
+			/* Don't print ',' & ' ' at the last count*/
+			if (i != 98 || j != 99)
+				break;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
