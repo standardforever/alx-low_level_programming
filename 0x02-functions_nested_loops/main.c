5a09a40d5d6c1c2f46f640d5bce9393496166aeb
standardforever <standard.forever123@gmail.com>
2022-04-30 06:11:29 +0000

    updated

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -8,6 +8,12 @@
  */
 int main(void)
 {
-	times_table();
-	return (0);
+	int r;
+
+    print_last_digit(98);
+    print_last_digit(0);
+    r = print_last_digit(-1024);
+    _putchar('0' + r);
+    _putchar('\n');
+    return (0);
 }
