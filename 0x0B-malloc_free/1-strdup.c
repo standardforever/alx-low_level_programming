7f2e45cf8b625b9c62f3f56d6724d35afc564f38
standardforever <standard.forever123@gmail.com>
2022-05-05 17:33:33 +0000

    updated

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,7 +18,7 @@ char *_strdup(char *str)
 	i = 0;
 	while (str[i] != '\0')
 		i++;
-	s = malloc(sizeof(char) * (i));
+	s = malloc(sizeof(char) * (i + 1));
 	for (i = 0; str[i] != '\0'; i++)
 		s[i] = str[i];
 	s[i] = '\0';
