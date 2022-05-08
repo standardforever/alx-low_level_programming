77d60188d7109e5850714293c03bea8832e65a92
standardforever <standard.forever123@gmail.com>
2022-05-08 01:35:59 -0700

    updated

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,19 +9,18 @@ void puts_half(char *str)
 {
 	int i, k;
 
+	k = 0;
 	/* find the length of the string */
 	i = 0;
 	while (str[i] != '\0')
 		i++;
-	/* divide the length to two half */
+	/* divide the length to two */
 	if (i % 2 == 0)
 		k = i / 2;
 	else
 		k = (i - 1) / 2;
-
 	/* print the last half of the string */
-	printf("%i", k);
-	for (k = k; str[k] != '\0'; k++)
+	for (k = k; k <= i - 1; k++)
 		_putchar(str[k]);
 	_putchar('\n');
 }
