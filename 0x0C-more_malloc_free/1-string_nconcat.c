c687b610b16e111818cdd925d12f2f1625994c3e
standardforever <standard.forever123@gmail.com>
2022-05-09 21:49:14 +0100

    updated

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,49 +1,42 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * string_nconcat - it concatenate two strings
- * @s1: the first string
- * @s2: the second string to be concatenate to s1
- * @n: the number of byte from s2 that will be concatenated to si
- * Description: if n is greater or equal to the length of s2 use the
- *              entire string s2. if NUll is passed treat as empty string
- * Return: if function fail it should return NULL, else the returned pointer
- *        shall point to the newly allocated space in memory, which contains s1
- *        followed by s2 and null terminated.
+ * string_nconcat - concatenates two strings.
+ * @s1: first string to copy
+ * @s2: second string to copy
+ * @n: number of bytes of s2 to copy
+ *
+ * Return: char pointer to newly allocated place in memory
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, k, add;
-	char *concat;
+	unsigned int i, j, k;
+	char *s;
 
-	add = 0;
-	/* if Null character is passed treat as an empty string*/
 	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	/* find the lenth of the string */
-	for (i = 0; s1[i] != '\0'; i++)
-		;
-	for (j = 0; s2[j] != '\0'; j++)
-		;
-	/* check if n is greater or equal to s2 */
-	if (n > j)
-	{
-		n = j;
-		/* add the length of the string */
-		add = j + i;
-	}
+		i = 0;
 	else
-		add = i + n;
-	/* allocate the required memory using malloc */
-	concat = malloc(sizeof(char) * add);
-	/*concatenate the two strings */
-	for (i = 0; s1[i] != '\0'; i++)
-		concat[i] = s1[i];
-	for (k = 0; k < n; k++, i++)
-		concat[i] = s2[k];
-	concat[i] = '\0';
-	return (concat);
+	{
+		for (i = 0; s1[i]; i++)
+			;
+	}
+	if (s2 == NULL)
+		j = 0;
+	else
+	{
+		for (j = 0; s2[j]; j++)
+			;
+	}
+	if (j > n)
+		j = n;
+	s = malloc(sizeof(char) * (i + j + 1));
+	if (s == NULL)
+		return (NULL);
+	for (k = 0; k < i; k++)
+		s[k] = s1[k];
+	for (k = 0; k < j; k++)
+		s[k + i] = s2[k];
+	s[i + j] = '\0';
+	return (s);
 }
--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,39 +1,24 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * _memset - copy char
- * @s: string
- * @b: input
- * @n: bytes
- * Return: string
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
-	return (s);
-}
-
-/**
- * _calloc - allocates memory for an array using malloc
- * @nmemb: n elements
- * @size: bytes
- * Return: pointer
+ * _calloc - allocates memory for an array, using malloc.
+ * @nmemb: number of elements in the array
+ * @size: size in bytes of the elements
+ *
+ * Return: void pointer to allocated memory
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p;
+	char *p;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(nmemb * size);
-
 	if (p == NULL)
 		return (NULL);
-	_memset(p, 0, (nmemb * size));
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
 	return (p);
 }
