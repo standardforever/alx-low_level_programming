7b47c222f91372d63263a9a2a048005259247571
standardforever <standard.forever123@gmail.com>
2022-05-11 22:10:40 +0000

    updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,9 +9,12 @@
  * Description: a structure with the name, age and owner of the dog.
  */
 
-struct dog {
-	char *name;
-	float age;
-	char *owner;
+struct dog
+{
+char *name;
+float age;
+char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif
