8d87534584c329c4c9b04a1622a8a1a7620619ce
standardforever <standard.forever123@gmail.com>
2022-05-11 12:34:33 +0000

    updated

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 /**
- * 2-main - it print the name of the file it was compiled from
- * Return - 0
+ * main - it print the name of the file it was compiled from
+ * Return:  0
  */
 int main(void)
 {
