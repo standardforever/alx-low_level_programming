c87a4688167d9b3f44c56cbd08b88d6f5144b4d7
standardforever <standard.forever123@gmail.com>
2022-05-13 15:41:19 -0700

    updated

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,10 +13,13 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	long unsigned int i;
 
-	i = 0;
-	while (i < size)
+	if (array && size && action)
 	{
-		action(array[i]);
-		i++;
+		i = 0;
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
 	}
 }
