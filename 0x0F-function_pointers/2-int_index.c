f28879f598e66805792348a1a15f26852e67c090
standardforever <standard.forever123@gmail.com>
2022-05-13 15:53:01 -0700

    updated

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,7 +5,7 @@
  * @array: the array to searche on
  * @size: the size of the array
  * @cmp: is a pointer to the function to be used to compare values
- * Retun: if size <= 0, return -1. if no element matches, return -1.
+ * Return: if size <= 0, return -1. if no element matches, return -1.
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
@@ -13,12 +13,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
-	i = 0;
-	while (array[i] != '\0')
+	if (array && size && cmp)
 	{
-		if (cmp(array[i]) == 1)
-			break;
-		i++;
+		i = 0;
+		while (i < size)
+		{
+			if (cmp(array[i]) == 1)
+				break;
+			i++;
+		}
 	}
-	return (1);
+	return (i);
 }
