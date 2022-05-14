f8a847086a1f4d0d366106319a12c01fc66a9dd8
standardforever <standard.forever123@gmail.com>
2022-05-14 10:26:26 -0700

    updated

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,3 +7,27 @@
  *		 ?, ", (, ), {, and }. will be capitalize.
  * Return: the capitalized string
  */
+char *cap_string(char *s)
+{
+	int i, j;
+
+	char c [14] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '\"', '(', };
+
+	cr[11] = '{';
+	cr[12] = '}';
+	cr[13] = ')';
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if (cr[j] == s[i])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] = *(s + (i + 1)) - 32;
+				}
+			}
+		}
+	}
+	return (s);
+}
