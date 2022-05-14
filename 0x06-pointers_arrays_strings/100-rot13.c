66faf83fbd466aec8fa746fbc410d8ecaa67ace6
standardforever <standard.forever123@gmail.com>
2022-05-14 14:03:04 +0000

    updated

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -5,7 +5,7 @@
  * @s: the string to encode
  * Description: Applying ROT13 to a piece of text merely requires examining
  *	    its alphabetic characters and replacing each one by the letter 13
- *	    places further along in the alphabet, wrapping back to the 
+ *	    places further along in the alphabet, wrapping back to the
  *	    beginning if necessary.[2] A becomes N, B becomes O, and so on up
  *	    to M, which becomes Z, then the sequence continues at the beginning
  *	    of the alphabet: N becomes A, O becomes B, and so on to Z, which
