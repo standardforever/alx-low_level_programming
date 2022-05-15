4a14dee90c7f1747ae74f590ce08f8cb24c844c4
standardforever <standard.forever123@gmail.com>
2022-05-15 21:16:54 +0000

    updated

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strstr - it locate a substring
+ * @haystack: the string to search
+ * @needle: the second string to search for the first occurrence
+ * Description: The _strstr() function finds the first occurrence of the
+ *              substring needle in the string haystack. The terminating
+ *              null bytes (\0) are not compared
+ * Return: Returns a pointer to the beginning of the located substring,
+ *          or NULL if the substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	
+}
