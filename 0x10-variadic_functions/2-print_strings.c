8b60b3f91fc7aac65d57ad5a47bad476574ca7db
standardforever <standard.forever123@gmail.com>
2022-05-16 15:20:53 +0000

    updated

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,7 +17,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(op, char *);
-		if (str != NULL)
+		if (str)
 			printf("%s", str);
 		else
 			printf("%s", "(nil)");
