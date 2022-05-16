093a81b22714a9de84a4edbff2383ad313078f2c
standardforever <standard.forever123@gmail.com>
2022-05-16 14:34:43 +0000

    updated

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,6 +2,8 @@
 #define _VARIADIC_FUNCTION_H_
 
 #include <stdarg.h>
+#include <stdio.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
