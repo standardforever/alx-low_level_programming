f75226647ac03489313b1baa6e58c789a93d0408
standardforever <standard.forever123@gmail.com>
2022-07-29 00:51:10 -0400

    updated

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hash_table;
 	unsigned long int i;
 
-	hash_table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	hash_table = (hash_table_t *)malloc(sizeof(hash_table_t *));
 	if (hash_table == NULL)
 		return (NULL);
 	hash_table->size = size;
