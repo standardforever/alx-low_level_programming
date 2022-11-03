2c4fbf591ae9249e27da1ba82afa014ab102363a
kusimosemiu@gmail.com <standard.forever123@gmail.com>
2022-11-02 22:13:37 -0400

    updated

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -14,6 +14,8 @@ int jump_search(int *array, size_t size, int value)
 	int jump = sqrt(size);
 	size_t i, x, prev = 0, next = 0;
 
+	if (array == NULL || size < 0)
+		return (-1);
 	for (i = 0; i < size; i += jump)
 	{
 		if (array[i] >= value)
